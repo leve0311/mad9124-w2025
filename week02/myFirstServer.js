fe0abb9f84cab33165a709836db1f09186f46d56
Taylor Levesque <leve0311@algonquinlive.com>
2025-01-17 18:13:21 -0500

    saving firstServer

--- week02/myFirstServer.js
+++ week02/myFirstServer.js
@@ -1,14 +1,19 @@
 "use strict";
 
 const http = require("http");
+const students = require("../week01/exercise/students.json");
 
 const server = http.createServer((request, response) => {
   if (request.url === "/feed") {
     response.write("This is your news feed");
   } else if (request.url === "/username") {
     response.write("tim");
+  } else if (request.url === "/api") {
+    response.setHeader("Content-type", "application/json");
+    response.write(JSON.stringify({ data: students }));
   } else {
-    response.write("Hello world from Node.js");
+    response.setHeader("Content-type", "application/json");
+    response.write(JSON.stringify({ error: "url not found" }));
   }
   response.end();
   //   console.log(request.url, request.method);
