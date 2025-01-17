371e7de19f52bcf45581cea7b68cf7086968f8cb
Taylor Levesque <leve0311@algonquinlive.com>
2025-01-17 18:14:11 -0500

    npm server stuffs week2

---
+++ week02/firstNpm/server.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const lodash = require("lodash");
+const students = require("../../week01/exercise/students.json");
+
+console.log(students);
+console.log(lodash.reverse(students));
